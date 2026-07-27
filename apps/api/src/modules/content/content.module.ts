fbdc583a2acf13bdd8f36b031bac6958b9502c81
Niz46 <paujohn66@gmail.com>
2026-07-27 11:49:18 +0100

    feat: enhance .gitignore and add tsconfig.build.json for improved project structure

--- apps/api/src/modules/content/content.module.ts
+++ apps/api/src/modules/content/content.module.ts
@@ -3,9 +3,11 @@ import { JikanService } from "./jikan/jikan.service";
 import { AniListService } from "./anilist/anilist.service";
 import { TmdbService } from "./tmdb/tmdb.service";
 import { ContentSyncProcessor } from "../../jobs/content-sync.processor";
-import { ContentController } from "./content.controller"
+import { ContentController } from "./content.controller";
+import { QueueModule } from "../../common/queue.module";
 
 @Module({
+  imports: [QueueModule],
   controllers: [ContentController],
   providers: [JikanService, AniListService, TmdbService, ContentSyncProcessor],
   exports: [JikanService, AniListService, TmdbService],
