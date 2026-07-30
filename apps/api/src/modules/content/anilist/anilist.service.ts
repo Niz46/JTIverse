2e383f3c56e22696976f2e79c3dec61a4365ce67
Niz46 <paujohn66@gmail.com>
2026-07-30 07:26:27 +0100

    feat(anilist): implement AniList ingestion service for media synchronization

--- apps/api/src/modules/content/anilist/anilist.service.ts
+++ apps/api/src/modules/content/anilist/anilist.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, Logger } from '@nestjs/common';
-import { PrismaService } from '../../../../common/prisma.service';
+import { PrismaService } from '../../../common/prisma.service';
 
 /**
  * ANILIST INGESTION SERVICE
